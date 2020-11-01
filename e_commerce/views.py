11cf09bb7d1cc79e0c9b48f17fb7d80ae03a2cbb
CaduLima <carlos.lima.exe@gmail.com>
2020-11-01 15:43:26 -0300

    add card, navbar and logout

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,48 +1,52 @@
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import authenticate, login, logout, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 from .forms import ContactForm, LoginForm, RegisterForm
 
+
 def home_page(request):
     context = {
-                    "title": "Home Page",
-                    "content": "Bem vindo a Home Page",
-              }
+        "title": "Home Page",
+        "content": "Bem vindo a Home Page",
+    }
     if request.user.is_authenticated:
         context["premium_content"] = "Você é um usuário Premium"
     return render(request, "home_page.html", context)
-    
+
+
 def about_page(request):
     context = {
-                    "title": "About Page",
-                    "content": "Bem vindo a About Page"
-              }
+        "title": "About Page",
+        "content": "Bem vindo a About Page"
+    }
     return render(request, "about/view.html", context)
+
 
 def contact_page(request):
     contact_form = ContactForm(request.POST or None)
     context = {
-                    "title": "Contact Page",
-                    "content": "Bem vindo a Contact Page",
-                    "form": contact_form
-              }
+        "title": "Contact Page",
+        "content": "Bem vindo a Contact Page",
+        "form": contact_form
+    }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
     return render(request, "contact/view.html", context)
 
+
 def login_page(request):
     form = LoginForm(request.POST or None)
     context = {
-                    "form": form
-              }
+        "form": form
+    }
     print("User logged in")
     print(request.user.is_authenticated)
     if form.is_valid():
         print(form.cleaned_data)
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password) 
+        user = authenticate(request, username=username, password=password)
         print(user)
         print(request.user.is_authenticated)
         if user is not None:
@@ -53,16 +57,27 @@ def login_page(request):
             # Redireciona para uma página de sucesso.
             return redirect("/")
         else:
-            #Retorna uma mensagem de erro de 'invalid login'.
+            # Retorna uma mensagem de erro de 'invalid login'.
             print("Login inválido")
     return render(request, "auth/login.html", context)
 
+
+def logout_page(request):
+    context = {
+                "content": "Você efetuou o logout com sucesso! :)"
+              }
+    logout(request)
+    return render(request, "auth/logout.html", context)
+
+
 User = get_user_model()
+
+
 def register_page(request):
     form = RegisterForm(request.POST or None)
     context = {
-                    "form": form
-              }
+        "form": form
+    }
     if form.is_valid():
         print(form.cleaned_data)
         username = form.cleaned_data.get("username")
